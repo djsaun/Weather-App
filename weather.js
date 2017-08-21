7ed33f01dcc44b67b30b7d4eb369e9e0227a084a
David Saunders <david@monkee-boy.com>
2017-08-21 08:21:16 -0500

    Print out current temperature and heat index details for city

--- weather.js
+++ weather.js
@@ -7,9 +7,24 @@ const api_key = require('./api.json');
 
 function getWeather(query) {
   const weatherRequest = https.get(`https://api.wunderground.com/api/${api_key.key}/conditions/q/${query}.json`, response => {
+
+    let body = "";
+
     response.on('data', data => {
-      console.log(data.toString());
+      body += data;
     });
+
+    response.on('error', error => {
+      console.log(error.message);
+    })
+
+    response.on('end', () => {
+      const bodyContent = body.toString();
+      const parsedContent = JSON.parse(bodyContent);
+      console.log(parsedContent['current_observation']);
+
+      console.log(`The current temperature for ${parsedContent["current_observation"]["display_location"]["full"]} is ${parsedContent["current_observation"]["temp_f"]} degrees Fahrenheit with a heat index of ${parsedContent["current_observation"]["heat_index_f"]} degrees Fahrenheit.`)
+    })
   })
 }
 
